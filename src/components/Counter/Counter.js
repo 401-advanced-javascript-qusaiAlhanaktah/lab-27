5208bbe1ddc06d7db0246b9e9a393fd8bea0ca99
Qusai-alhanaktah <qusaialhanaktah@gmail.com>
2020-02-24 15:28:05 +0200

    working on the lab

---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+
+class Counter extends React.Component {
+    constructor(properties) {
+      super(properties)
+      this.counter = 0;
+    }
+   
+    handLeIncrementClick = e => {
+      e.preventDefault();
+      let counter = this.counter++;
+      // console.log(counter);
+      this.setState({counter: counter++})
+  }
+  handLeDecrementClick = e => {
+    e.preventDefault();
+    // let counter = e.target.value;
+    let counter = this.counter--;
+    // console.log(counter);
+    this.setState({counter: counter++})
+  }
+  
+  render() {
+    return (
+      <section>
+        <h3>{this.counter}</h3>
+        <button className="button" onClick={this.handLeIncrementClick}>click for Increment</button>
+        <button className="button" onClick={this.handLeDecrementClick}>click for Decrement</button>
+      </section>
+    )
+  }
+  }
+
+export default Counter;
